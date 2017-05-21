269e0c8b48814997dc1fda3d10c45caa69122efb
reda <reda@localhost>
2017-05-21 17:08:30 +0300

    playlist building

--- zayats_tv.py
+++ zayats_tv.py
@@ -15,8 +15,9 @@ CONFIG_FILE = os.path.join(WORK_DIR, 'config')
 WATCHED_FILE = os.path.join(WORK_DIR, 'watched')
 
 WAIT_AFTER_MOUNT_SECS = 5
-LAUNCH_PLAYER = 'mpv'
+PLAYLIST_ITERATIONS = 6
 
+NONE_SHOW = 'NONE'
 
 # ----cfg-----------
 MAX_ACTIVE_SERIALS = 'max_active_serials'
@@ -37,13 +38,12 @@ last_watched = {}
 
 path_to_serials = None
 path_to_adv = None
-
-playlist_serials = []
+max_active_serials = 0
 
 # ----- init module-----
 random.seed()
 LOG_FORMAT = '%(asctime)s %(message)s'
-logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG, format=LOG_FORMAT)
 
 
 def readpath(path, base):
@@ -62,9 +62,14 @@ def read_config():
     if config.has_option(SECTION, PATH_TO_ADV):
         global path_to_adv
         path_to_adv = readpath(config.get(SECTION, PATH_TO_ADV), dirbase)
+    global max_active_serials
+    max_active_serials = int(config.get(SECTION, MAX_ACTIVE_SERIALS))
 
 
 def read_state():
+    if not os.path.exists(WATCHED_FILE):
+        logging.warn('NO WATCHED LIST: ' + WATCHED_FILE + ' USE EMPTY')
+        return
     with open(WATCHED_FILE) as f:
         data = eval(f.read())
     global active_serials
@@ -106,6 +111,8 @@ def adv_gener():
 
 
 def episodes_in_season_gener(serial):
+    if not serial:
+        return
     sp = os.path.join(path_to_serials, serial)
     all_episodes = sorted(list_files(sp))
     cur_season = None
@@ -118,51 +125,112 @@ def episodes_in_season_gener(serial):
             cur_season = season
 
 
+def choose_serials_in_playlist():
+    global active_serials
+    serials = set(os.listdir(path_to_serials))
+    target = list(serials - set(active_serials))
+    random.shuffle(target)
+    if len(active_serials) > max_active_serials:
+        active_serials = active_serials[:max_active_serials]
+    else:
+        active_serials.extend([None]*(max_active_serials-len(active_serials)))
+    j = 0
+    for i in range(len(active_serials)):
+        if active_serials[i] == None:
+            while j < len(target):
+                serial = target[j]
+                gen = episodes_in_season_gener(serial)
+                try:
+                    next(gen)
+                    active_serials[i] = target[j]
+                    j += 1
+                    break
+                except StopIteration:
+                    j += 1    # That serial has ended, try next
+
+
+def stop_if_season_ended():
+    global active_serials
+    for i in range(len(active_serials)):
+        serial = active_serials[i]
+        lw = last_watched.get(serial, None)
+        if lw:
+            gen = episodes_in_season_gener(serial)
+            try:
+                ep = next(gen)
+                season1 = ep[:ep.rfind(os.sep)]
+                season2 = lw[:lw.rfind(os.sep)]
+                if season1 != season2:
+                    active_serials[i] = None
+            except StopIteration:
+                active_serials[i] = None  # NO more episodes
+
+
 def make_play_list():
     read_state()
-    playlist_serials = list(active_serials)
-    serials = set(os.listdir(path_to_serials))
-    target = list(serials - set(playlist_serials))
-    random.shuffle(target)
-    need_serials = int(config.get(SECTION, MAX_ACTIVE_SERIALS)) - len(playlist_serials)
-    playlist_serials.extend(target[:min(need_serials, len(target))])
+    logging.info('ACTIVE SERIALS: ' + str(active_serials))
+    stop_if_season_ended()
+    logging.info('FILTER RECENTLY ENDED SEASONS: ' + str(active_serials))
     result = []
     adv_gen = adv_gener()
-    gens = [episodes_in_season_gener(serial) for serial in playlist_serials]
-    for gen in gens:
-            try:
-                result.append(next(gen))
-            except StopIteration:
-                continue
-            for _ in range(int(config.get(SECTION, ADV_PER_PAUSE))):
+    gens = [None] * max_active_serials
+    for epoch in range(PLAYLIST_ITERATIONS):
+        oldlist = list(active_serials)
+        choose_serials_in_playlist()
+        for i in range(max_active_serials):
+            if (gens[i] == None) or (i >= len(oldlist)) or (oldlist[i] != active_serials[i]):
+                gens[i] = episodes_in_season_gener(active_serials[i])
+        logging.info('PLAYLIST_SERIALS: ' + str(active_serials))
+        for i in range(len(active_serials)):
                 try:
-                    result.append(next(adv_gen))
+                    ep = next(gens[i])
+                    last_watched[active_serials[i]] = ep
+                    result.append(ep)
+                    logging.debug(ep)
                 except StopIteration:
-                    pass      # NO ADV
+                    result.append(NONE_SHOW)  # NO SHOW. Empty Record
+                    active_serials[i] = None
+                    gens[i] = None
+                    continue
+                for _ in range(int(config.get(SECTION, ADV_PER_PAUSE))):
+                    try:
+                        result.append(next(adv_gen))
+                    except StopIteration:
+                        break      # NO ADV
     return result
 
 
 def watch():
+    pos = 0
+    watching = None
     for line in sys.stdin.readlines():
         ind = line.find('Playing: ')
         if ind != -1:
+            if watching:                                 # We have watched an episode
+                last_watched[watching[0]] = watching[1]
+                watching = None
             playing = line[ind+9:]
             playing = playing.strip()
+            if playing == NONE_SHOW:
+                active_serials[pos] = None
+                pos = (pos + 1) % max_active_serials
             if playing.startswith(path_to_serials):
                 start = len(path_to_serials)
                 end = playing.find(os.sep, start+1)
                 serial = playing[start:end]
                 serial = serial.replace(os.sep, '')
-                print 'SERIAL: '+serial+' EP: '+playing+'!!!!'
+                active_serials[pos] = serial
+                watching = (serial, playing)
+                pos = (pos + 1) % max_active_serials
+    save_state()
 
 
 def main():
     read_config()
-    if '-watch' in sys.argv:
+    if ('-watch' in sys.argv):
         print('zayats-tv watching')
         watch()
     else:
-
         if not path_to_serials:
             logging.error('path_to_serials is not specified')
         mount_if_neccessery(path_to_serials)
@@ -170,7 +238,6 @@ def main():
             mount_if_neccessery(path_to_adv)
         else:
             logging.warn('path_to_adv is not specified')
-
         for line in make_play_list():
             print(line)
 
--- zayats_tv.sh
+++ zayats_tv.sh
@@ -3,5 +3,5 @@
 python zayats_tv.py > playlist.m3u
 script -f -c 'mpv --playlist playlist.m3u 2>/dev/null' /dev/null | tee watch.log
 cat watch.log | python zayats_tv.py -watch
-#rm watch.log
+rm watch.log
 
