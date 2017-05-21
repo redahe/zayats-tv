933609d876a3630aceea5fcc2c639c59f5f24997
reda <reda@localhost>
2017-05-21 19:54:23 +0300

    Change README

--- zayats_tv.sh
+++ zayats_tv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+rm playlist.m3u
 python zayats_tv.py > playlist.m3u
-script -f -c 'mpv --playlist playlist.m3u 2>/dev/null' /dev/null | tee watch.log
+DISPLAY=:0 script -f -c 'mpv --fs --playlist playlist.m3u 2>/dev/null' /dev/null | tee watch.log
 cat watch.log | python zayats_tv.py -watch
 rm watch.log
 
