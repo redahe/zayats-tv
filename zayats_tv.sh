032594e09d4326327cd0c0864d3b069293ef1c2d
redahe <redaworld@gmail.com>
2017-05-30 20:32:20 +0000

    changed sh

--- zayats_tv.sh
+++ zayats_tv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-python zayats_tv.py > playlist.m3u
-script -f -c 'mpv --playlist playlist.m3u 2>/dev/null' /dev/null | tee watch.log
-cat watch.log | python zayats_tv.py -watch
+rm playlist.m3u
 rm watch.log
+
+python zayats_tv.py > playlist.m3u
+DISPLAY=:0 script -f -c 'mpv --fs --playlist playlist.m3u 2>/dev/null' /dev/null | tee watch.log
+cat watch.log | python zayats_tv.py -watch
 
